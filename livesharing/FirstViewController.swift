19f96ff97ab2ee5ce1c0f9733885f1784d9ef540
masatsch <masato.11.soccer@gmail.com>
2019-09-01 13:47:46 +0900

    Initial Commit

---
+++ livesharing/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  livesharing
+//
+//  Created by Masato TSUTSUMI on 2019/09/01.
+//  Copyright © 2019 Masato TSUTSUMI. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ livesharing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  livesharing
+//
+//  Created by Masato TSUTSUMI on 2019/09/01.
+//  Copyright © 2019 Masato TSUTSUMI. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
