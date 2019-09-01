e6a38828573770bff95da22276a4c82bce81eb5a
masatsch <masato.11.soccer@gmail.com>
2019-09-01 18:49:12 +0900

    wip

--- livesharing/View/Calendar/CalendarViewController.swift
+++ livesharing/View/Calendar/CalendarViewController.swift
@@ -11,9 +11,6 @@ import UIKit
 
 
 class CalendarViewController: UIViewController {
-    @IBOutlet weak var calendarNavigationItem: UINavigationItem!
-    @IBOutlet weak var dayButton: UIButton!
-    @IBOutlet weak var calendarCollectionViewCell: UICollectionViewCell!
     
     override func viewDidLoad() {
         super.viewDidLoad()
