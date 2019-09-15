cdf8d21dffc0887105088ff090eb2b363df96d69
masatsch <masato.11.soccer@gmail.com>
2019-09-15 18:26:04 +0900

    artist api method

--- livesharing/APIRequest/ArtistAPIRequest.swift
+++ livesharing/APIRequest/ArtistAPIRequest.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
+
+
+struct ArtistAPIRequest {
+    func getArtist(id: String) {
+        let db = Firestore.firestore()
+        db.collection("artists").document(id).getDocument() { (snap, error) in
+            if let error = error {
+                fatalError("\(error)")
+            } else {
+                guard let document = snap?.data() else { return }
+                print(document)
+            }
+        }
+    }
+    
+    func getArtists() {
+        let db = Firestore.firestore()
+        db.collection("artists").getDocuments() { (snaps, error) in
+            if let error = error {
+                fatalError("\(error)")
+            } else {
+                guard let documents = snaps?.documents else { return }
+                print(documents)
+            }
+        }
+    }
+    
+    func registerNewArtist(data: [String: Any]) {
+        let db = Firestore.firestore()
+        db.collection("artists").addDocument(data: data) { error in
+            if let error = error {
+                print("\(error)")
+            } else {
+                return
+            }
+        }
+    }
+    
+    func favorite(id: String) {
+        let db = Firestore.firestore()
+        db.collection("artists").document(id).updateData(["isFavorite": true]) { error in
+            if let error = error {
+                print("\(error)")
+            } else {
+                return
+            }
+        }
+    }
+    
+    func unfavorite() {
+        let db = Firestore.firestore()
+        db.collection("artists").document(id).updateData(["isFavorite": false]) { error in
+            if let error = error {
+                print("\(error)")
+            } else {
+                return
+            }
+        }
+    }
+}
