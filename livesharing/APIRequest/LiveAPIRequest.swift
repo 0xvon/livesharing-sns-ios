776e9eeb7b9f9ed1edd826c34b8d3f1e5aa82c9b
masatsch <masato.11.soccer@gmail.com>
2019-09-15 18:43:26 +0900

    notification api method

--- livesharing/APIRequest/LiveAPIRequest.swift
+++ livesharing/APIRequest/LiveAPIRequest.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+import Firebase
+
+
+struct LiveAPIRequest {
+    func getLive(id: String) {
+        let db = Firestore.firestore()
+        db.collection("lives").document(id).getDocument() { (snap, error) in
+            if let error = error {
+                fatalError("\(error)")
+            } else {
+                guard let document = snap?.data() else { return }
+                print(document)
+            }
+        }
+    }
+    
+    func getLives() {
+        let db = Firestore.firestore()
+        db.collection("lives").getDocuments() { (snaps, error) in
+            if let error = error {
+                fatalError("\(error)")
+            } else {
+                guard let documents = snaps?.documents else { return }
+                print(documents)
+            }
+        }
+    }
+    
+    func getSetlist(id: String) {
+        let db = Firestore.firestore()
+        db.collection("musics").whereField("liveId", isEqualTo: id).getDocuments() { (snaps, error) in
+            if let error = error {
+                fatalError("\(error)")
+            } else {
+                guard let documents = snaps?.documents else { return }
+                print(documents)
+            }
+        }
+    }
+    
+    func join(id: String) {
+        let db = Firestore.firestore()
+        db.collection("lives").document(id).updateData(["isJoin": true]) { error in
+            if let error = error {
+                print("\(error)")
+            } else {
+                return
+            }
+        }
+    }
+    
+    func unjoin(id: String) {
+        let db = Firestore.firestore()
+        db.collection("lives").document(id).updateData(["isJoin": false]) { error in
+            if let error = error {
+                print("\(error)")
+            } else {
+                return
+            }
+        }
+    }
+}
--- livesharing/APIRequest/NotificationAPIRequest.swift
+++ livesharing/APIRequest/NotificationAPIRequest.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import Firebase
+
+
+struct NotificationAPIRequest {
+    func getNotification() {
+        let db = Firestore.firestore()
+        db.collection("notification").getDocuments() { (snaps, error) in
+            if let error = error {
+                print("\(error)")
+            } else {
+                return
+            }
+        }
+        //        TODO: push notification method
+    }
+    
+    func readNotification(id: String) {
+        let db = Firestore.firestore()
+        db.collection("notification").document(id).updateData(["isRead": true]) { error in
+            if let error = error {
+                print("\(error)")
+            } else {
+                return
+            }
+        }
+    }
+}
